76f8762049fdca1dab26152803f7d7357f6db2cf
NessMC <contact@nessmc.fr>
2021-01-31 23:16:18 +0100

    🔧 Basic tokens added.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
-import { Lexer } from './core/lexer';
+import {Lexer} from './core/lexer';
 import fs from 'fs/promises';
 
 async function main(): Promise<void> {
-  const content: string = await fs.readFile('./example/main.qrk', 'utf-8');
-  Lexer.tokenize(content);
-}
+  Lexer.definition = {
+    Comment: /--.*/,
+    Type: /(Natural|Real)/,
+    Arguments: /=>/,
+    Operations: /(=|\+|-|\\)/,
+    Integer: /(\d+(?:\.\d+)?)/,
+    String: /".*?"/,
+    Word: /\w+/,
+    Bracket: /(\[|\])/,
+    Range: /\.\./,
+  };
 
+  const content: string = await fs.readFile('./example/main.qrk', 'utf-8');
+  const tokens = content
+    .split(/\r?\n/g)
+    .filter((x) => x.trim().length > 0)
+    .map((line, index) => Lexer.tokenize(line).map((tok) => {
+      tok.line = index;
+      return tok;
+    }));
+  console.log(tokens);
+}
 main();
