ed96f7d6b9ed7b9983161ede986b1142e708b968
NessMC <contact@nessmc.fr>
2021-01-31 23:11:56 +0100

    ✨ Regex based Lexer added.

--- src/core/lexer.ts
+++ src/core/lexer.ts
@@ -1,65 +1,53 @@
 export class Lexer {
-  public static tokenize(code: string) {
-    const content: string[] = code
-      .split(/\r?\n/g)
-      .filter((line) => line.trim().length > 0);
-    const output: Token[][] = [];
-    
-    for (const indexLine in content) {
-      const line = content[indexLine];
-      const state: State = {
-        status: '',
-        tabs: true,
-        tokens: {
-          container: [],
-          tmp: '',
-        }
-      };
-      for (const charIndex in line as any) {
-        const char: string = line[charIndex];
-        if (char === ' ' && state.tabs) {
-          state.tokens.tmp += char;
-        } else {
-          if (state.tokens.tmp.length > 0 && state.tabs) {
-            state.tokens.container.push({ 
-              type: TokenTypes.Space,
-              value: state.tokens.tmp,
-              line: Number(indexLine),
-              position: Number(charIndex) - state.tokens.tmp.length,
-              length: state.tokens.tmp.length,
-            });
-            state.tokens.tmp = '';
-          }
-          state.tabs = false;
-          break;
-        }
-      }
-      output.push(state.tokens.container);
+  public static definition: Record<string, RegExp> = {};
+  private static tokens: Token[] = [];
+
+  private static matchNearestToken(content: string, slice: number): LexerMatch {
+    const top = Object
+      .entries(this.definition)
+      .map(([name, regex]) => ({
+        name,
+        regex: content.slice(slice).match(regex),
+      }));
+    const filtered = top.filter((x) => x.regex !== null);
+    const match = filtered.sort((a, b) => a.regex.index - b.regex.index)[0];
+    if (!match) return undefined;
+    match.regex.index += slice;
+    return match;
+  }
+
+  private static parseAsToken(match: LexerMatch): Token {
+    return {
+      type: match.name,
+      value: match.regex[0],
+      length: match.regex[0].length,
+      position: match.regex.index,
+      line: 0,
     }
-    console.log(output)
+  }
+
+  private static format(content: string, slice: number = 0) {
+    const match = this.matchNearestToken(content, slice);
+    if (!match) return this.tokens;
+    const matchSlice = match.regex.index + match.regex[0].length;
+    this.tokens.push(this.parseAsToken(match))
+    return this.format(content, matchSlice);
+  }
+
+  public static tokenize(content: string): Token[] {
+    this.tokens = [];
+    return this.format(content);
   }
 }
 
-interface State {
-  status: string,
-  tabs: boolean,
-  tokens: {
-    container: Token[],
-    tmp: string,
-  }
+interface LexerMatch {
+  name: string,
+  regex: RegExpMatchArray,
 }
-
-enum TokenTypes {
-  Space = 'Space',
-  Type = 'Type',
-  Keyword = 'Keyword',
-  Word = 'Word',
-}
-
 interface Token {
-  type: TokenTypes,
+  type: string,
   value: string,
-  line: number,
-  position: number,
   length: number,
+  position: number,
+  line: number,
 }
