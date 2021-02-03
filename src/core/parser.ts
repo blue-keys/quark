fce61ad7168c4848f98bc641317c3e087b66de57
thomasvergne <thomas@quark-lang.dev>
2021-02-03 13:28:27 +0100

    ✨ Tabdown parsing added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,14 +1,36 @@
 import { Token } from './lexer';
 
 export class Parser {
-  private static ast = {};
+  private static ast = {
+    type: 'Program',
+    body: [],
+  };
 
-  private static processAny() {
-    console.log(this.ast)
+  private static processNode(line: number, index: number, tokens: Token[][], ast) {
+    ast.body.push({
+      type: 'Node',
+      body: [],
+      parent: ast,
+    });
+    return this.processAny(line + 1, 0, tokens, ast.body.slice(-1)[0]);
   }
 
-  public static parse(tokens: Token[]): typeof Parser.ast {
-    console.log(this.ast)
-    return this.ast;
+  private static processAny(line: number, index: number, tokens: Token[][], ast) {
+    if (!tokens[line]) return this.ast;
+    const token: Token = tokens[line][index];
+    if (index === 0) {
+      if (!ast.tabs) ast.tabs = token.position;
+      if (token.position < ast.tabs) {
+        return this.processAny(line, index, tokens, ast.parent);
+      }
+    }
+    if (index >= tokens[line].length) return this.processAny(line + 1, 0, tokens, ast);
+    if (token.value === 'do') return this.processNode(line, index, tokens, ast);
+    return this.processAny(line, index + 1, tokens, ast);
+  }
+
+  public static parse(tokens: Token[][]): typeof Parser.ast {
+    this.processAny(0, 0, tokens, this.ast);
+    return this.ast.body.slice(-1)[0];
   }
 }
