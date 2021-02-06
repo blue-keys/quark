e30a4e9255d5b25abd4c46aa937872bc1121ce2a
thomasvergne <thomas@quark-lang.dev>
2021-02-06 10:52:09 +0100

    ✨ Node processing added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,32 +1,76 @@
 import { Token } from './lexer';
 
 class ParserCreator {
-  private ast: any = {};
+  private ast: any = {
+    type: 'Program',
+    body: [],
+  };
   private RULES: Record<string, string> = {};
   private readonly joinedTokens: Token[] = [];
 
   public createNodeWith(token: string[] | string) {
     if (!Array.isArray(token)) token = [token];
-    for (const tok of token) this.RULES[tok] = 'Node';
+    for (const tok of token) this.RULES[tok] = 'NodeCreator';
+    return null;
+  }
+
+  public closeNodeWith(token: string[] | string) {
+    if (!Array.isArray(token)) token = [token];
+    for (const tok of token) this.RULES[tok] = 'NodeDestructor';
     return null;
   }
 
   protected constructor(
     private readonly tokens: Token[][]
   ) {
-    for (const line of this.tokens) {
-      for (const token of line) {
-        this.joinedTokens.push(token);
-      }
-    }
+    this.tokens.map((line) => this.joinedTokens.push(...line));
   }
 
-  private process() {
-    console.log(this.joinedTokens);
+  private getRule(token: Token): string {
+    return this.RULES[token.type];
+  }
+
+  private process(ast: any = this.ast) {
+    for (const token of this.joinedTokens) {
+      const rule: string = this.getRule(token);
+      if (!rule) continue;
+      switch (rule) {
+        case 'NodeCreator':
+          ast.body.push({
+            type: 'Node',
+            body: [],
+            parent: ast,
+          });
+          ast = ast.body.slice(-1)[0];
+          break;
+
+        case 'NodeDestructor':
+          ast = ast.parent;
+          break;
+      }
+    }
+    return this.ast;
   }
 
   public output() {
-    return this.process();
+    const formatter = this.process();
+
+    const getCircularReplacer = () => {
+      const seen = new WeakSet();
+      return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+          if (seen.has(value)) {
+            return;
+          }
+          seen.add(value);
+        }
+        return value;
+      };
+    };
+
+    return JSON.parse(
+      JSON.stringify(formatter, getCircularReplacer(), 2)
+    );
   }
 }
 
