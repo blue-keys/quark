b134ec8946466166509f26a7c2e72986526d5d87
NessMC <contact@nessmc.fr>
2021-02-11 17:24:54 +0100

    ✨ Indentation based parser added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,100 +1,70 @@
 import { Token } from './lexer';
 
 class ParserCreator {
-  private ast: any[] = [];
+  private ast: any = { tabs: 0 };
   private RULES: Record<string, any> = {};
   private readonly joinedTokens: Token[] = [];
 
-  public createNodeWith(token: string[] | string) {
-    if (!Array.isArray(token)) token = [token];
-    for (const tok of token) this.RULES[tok] = 'NodeCreator';
-    return null;
-  }
-  public closeNodeWith(token: string[] | string) {
-    if (!Array.isArray(token)) token = [token];
-    for (const tok of token) this.RULES[tok] = 'NodeDestructor';
-    return null;
-  }
-
   protected constructor(
-    private readonly tokens: Token[][]
+    private readonly tokens: Token[][],
+    private readonly options: any,
   ) {
     this.tokens.map((line) => this.joinedTokens.push(...line));
-  }
-
-  private findParent(node: any, root: any = this.ast): any | null {
-    let found: any | null = null;
-    for (const child of root) {
-      if (child === node) return root;
-      if ('type' in child && 'value' in child) continue;
-      found = this.findParent(node, child);
-      if (found !== null) return found;
-    }
-    return null;
   }
 
   private getRule(token: Token): any {
     return this.RULES[token.type];
   }
 
-  public addRule(token: string[] | string, rule: (..._: any) => any) {
-    if (!Array.isArray(token)) token = [token];
-    for (const tok of token) this.RULES[tok] = rule;
-    return null;
+  private findParent(token: Token, ast) {
+    if (ast.tabs === token.position) return ast;
+    return this.findParent(token, ast.parent);
   }
 
-  private process(ast: any = this.ast) {
+  public node(opener: string, closer?: string) {
+    if (this.options.indentation || !closer) {
+      this.options.indentation = true;
+      this.RULES[opener] = 'NodeIndentation';
+    } else {
+      this.RULES[opener] = 'NodeCreator';
+      this.RULES[closer] = 'NodeDestructor';
+    }
+  }
+
+  public parse() {
+    let state: number = 0;
+    let ast = this.ast;
     for (const index in this.joinedTokens) {
       const token = this.joinedTokens[index];
-      const rule: string = this.getRule(token);
-      if (!rule) continue;
-      switch (rule) {
-        case 'NodeCreator':
-          ast.push([]);
-          ast = ast.slice(-1)[0];
-          break;
-        case 'NodeDestructor':
-          ast = this.findParent(ast, this.ast);
-          break;
-        default:
-          const fn = <(..._: any) => any><unknown>rule;
-          const res = fn(
-            token,
-            this.joinedTokens[Number(index) - 1],
-            this.joinedTokens[Number(index) + 1],
-            ast.slice(-1)[0],
-          );
-          if (res !== undefined) ast.push(res);
-          break;
+      const rule = this.getRule(token);
+      if (this.options.indentation) {
+        if (rule && rule === 'NodeIndentation') {
+          state = 1;
+          ast.body = [{ parent: ast, }];
+          continue;
+        }
+        if (state === 1) {
+          ast = ast.body.slice(-1)[0];
+          ast.tabs = token.position;
+          state = 0;
+        }
+        if (this.joinedTokens[Number(index) - 1]?.position > token.position && this.joinedTokens[Number(index) - 1]?.type !== 'Block') {
+          ast = this.findParent(token, ast);
+        }
+      }
+      if (token.type === 'Keyword') {
+        if (token.value === 'func') {
+          ast.type = 'FunctionDeclaration';
+        }
+      } else if (token.type === 'Word') {
       }
     }
-    return this.ast;
-  }
-
-  public output() {
-    const formatter = this.process();
-
-    const getCircularReplacer = () => {
-      const seen = new WeakSet();
-      return (key, value) => {
-        if (typeof value === "object" && value !== null) {
-          if (seen.has(value)) {
-            return;
-          }
-          seen.add(value);
-        }
-        return value;
-      };
-    };
-
-    return JSON.parse(
-      JSON.stringify(formatter, getCircularReplacer(), 2)
-    );
+    console.log(this.ast)
   }
 }
 
 export class Parser extends ParserCreator {
-  public static createParser(tokens: Token[][]) {
-    return new ParserCreator(tokens);
+  public static createParser(tokens: Token[][], options: any) {
+    return new ParserCreator(tokens, options);
   }
 }
