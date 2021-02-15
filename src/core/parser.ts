cc2d18d5dbc208ca016e39d557471b130b94ac7c
thomasvergne <thomas@quark-lang.dev>
2021-02-15 23:56:27 +0100

    ✨ AST Visitor methods added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -69,6 +69,56 @@ class ParserCreator {
       if (!rule) continue;
       rule(token, previous, next);
     }
+    return this.initAST();
+  }
+
+  private initAST() {
+    function isContainer(element: any): boolean {
+      return Array.isArray(element) && element.every((child) => Array.isArray(child));
+    }
+    const defineProperty = (name: string, value: any) => {
+      Object.defineProperty(this.ast, name, { writable: true, enumerable: false });
+      this.ast[name] = value;
+    };
+
+    // Output property
+    const getCircularReplacer = () => {
+      const seen = new WeakSet();
+      return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+          if (seen.has(value)) {
+            return;
+          }
+          seen.add(value);
+        }
+        return value;
+      };
+    };
+    function formatAST(ast) {
+      return JSON.parse(JSON.stringify(ast, getCircularReplacer()))
+    }
+
+    defineProperty('output', formatAST(this.ast));
+
+    // AST Visitor
+    defineProperty('visit', () => {
+      const visitor = (node) => {
+        if (isContainer(node)) {
+          for (const child of node) visitor(child);
+        } else {
+          const expression: string = node[0].value;
+          if (this.ast.handlers[expression]) {
+            this.ast.handlers[expression](node[0], formatAST(node), node);
+          }
+        }
+      };
+      visitor(this.ast);
+    });
+    defineProperty('handlers', {});
+    defineProperty('on', (type: string, callback) => {
+        this.ast.handlers[type] = callback;
+      });
+
     return this.ast;
   }
 }
